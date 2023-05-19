dd4b69363425ede46ff638c58ee51a97001d9772
Lelei <66555957+xLelei@users.noreply.github.com>
2023-05-19 13:07:07 +0200

    Agregadas operaciones CRUD al servicio de heroes

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, Observable, of } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { environment } from '../../../environments/environment';
 import { Hero } from '../interfaces/hero.interface';
 
@@ -13,25 +13,46 @@ export class HeroesService {
 
   constructor(private http: HttpClient) { }
 
+  // Métodos READ
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(`${this.baseUrl}/heroes`);
   }
 
   getHeroById(id: string): Observable<Hero | undefined> {
     return this.http
-      .get<Hero>(`${this.baseUrl}/heroes/${id}`)
-      .pipe(
-        catchError(error => of(undefined))
-      );
+               .get<Hero>(`${this.baseUrl}/heroes/${id}`)
+               .pipe(
+                 catchError(error => of(undefined))
+               );
   }
 
   getSuggestions(query: string): Observable<Hero[]> {
     let params: HttpParams = new HttpParams();
-    params = params.set('q',query);
-    params = params.set('_limit',6);
+    params = params.set('q', query);
+    params = params.set('_limit', 6);
     // params = params.append('q',query);
     // params = params.append('_limit',6);
     console.log({params});
     return this.http.get<Hero[]>(`${this.baseUrl}/heroes`, {params: params});
+  }
+
+  // Métodos CREATE
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(`${this.baseUrl}/heroes`, hero);
+  }
+
+  // Métodos UPDATE
+  updateHero(hero: Hero): Observable<Hero> {
+    if (!hero.id) throw Error('Hero id is required');
+    return this.http.patch<Hero>(`${this.baseUrl}/heroes/${hero.id}`, hero);
+  }
+
+  // Métodos DELETE
+  deleteHeroById(id: string): Observable<boolean> {
+    return this.http.delete(`${this.baseUrl}/heroes/${id}`)
+      .pipe(
+        catchError( err => of(false)),
+        map( resp => true)
+      );
   }
 }
